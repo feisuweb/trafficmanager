ad950fcdcb65079e7b7cf13dfa77f775eefd73e1
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-11-03 19:17:19 +0200

    Implement more app popup

--- app/src/main/java/team/monroe/org/trafficmanager/FragmentDashboardHeader.java
+++ app/src/main/java/team/monroe/org/trafficmanager/FragmentDashboardHeader.java
@@ -1,8 +1,12 @@
 package team.monroe.org.trafficmanager;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class FragmentDashboardHeader extends FragmentDashboardSupport {
@@ -10,6 +14,7 @@ public class FragmentDashboardHeader extends FragmentDashboardSupport {
     private TextView mTextCaption;
     private View mButtonBack;
     private ImageView mIcon;
+    private PopupWindow mMorePopup;
 
     @Override
     protected int getLayoutId() {
@@ -22,7 +27,43 @@ public class FragmentDashboardHeader extends FragmentDashboardSupport {
         mTextCaption = view(R.id.text_caption, TextView.class);
         mButtonBack = view(R.id.button_back);
         mIcon = view(R.id.image_icon, ImageView.class);
+        view(R.id.action_more, ImageButton.class).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onMoreMenuClick(v);
+            }
+        });
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMorePopup.dismiss();
+        mMorePopup = null;
+    }
+
+    private void onMoreMenuClick(View v) {
+        if (mMorePopup == null) {
+            View view = dashboard().getLayoutInflater().inflate(R.layout.panel_more, null);
+            view.findViewById(R.id.action_export_devices).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mMorePopup.dismiss();
+                    onExportDevices();
+                }
+            });
+            mMorePopup = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            mMorePopup.setBackgroundDrawable(new BitmapDrawable());
+            mMorePopup.setOutsideTouchable(true);
+        }
+        mMorePopup.showAsDropDown(v);
+    }
+
+    private void onExportDevices() {
+        
+    }
+
 
     public void update(ActivityDashboard.BodyPageId pageId, boolean stepBackSupport) {
         mButtonBack.setVisibility(stepBackSupport ? View.VISIBLE : View.INVISIBLE);
